96fd5092ea642f71d33755da0a2a3210cf6073f0
Ktjj91 <kingsleythomas33@gmail.com>
2024-11-05 16:40:47 +0100

    api platform + jwt

--- src/Entity/Attribut.php
+++ src/Entity/Attribut.php
@@ -2,12 +2,15 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AttributRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AttributRepository::class)]
+#[ApiResource]
+
 class Attribut
 {
     #[ORM\Id]
